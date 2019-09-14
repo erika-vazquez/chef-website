41c188686154023f2f146a7142cfc7ac36382bc0
Erika <ito2016@protonmail.ch>
2019-09-13 22:39:17 -0400

    Added more styling with semantic-ui-react

--- chef-site/src/components/homeSection.js
+++ chef-site/src/components/homeSection.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+class HomeSec extends Component {
+	render() {
+		return (
+			<>
+      <h1>HELLO</h1>
+        </>
+		)
+	}
+}
+
+export default HomeSec;
