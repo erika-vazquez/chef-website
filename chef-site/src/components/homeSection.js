82fdfe8f0bd8b79a2395dcd0f37e674d6de57fdd
Erika <ito2016@protonmail.ch>
2019-09-16 23:08:58 -0400

    Added footer section

--- chef-site/src/components/homeSection.js
+++ chef-site/src/components/homeSection.js
@@ -7,6 +7,7 @@ class HomeSec extends Component {
 			<img src="/Pictures/mediterranean-cuisine-2378758_1920.jpg" ></img>
       <h1>iCook4U Restaurant</h1>
 	  <h2>Some slogan here</h2>
+	  <footer>By Erika I. Vazquez</footer>
         </>
 		)
 	}
