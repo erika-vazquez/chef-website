41a453052cb4afbf9ba51a461061f9d04262e311
Erika <ito2016@protonmail.ch>
2019-09-17 22:30:34 -0400

    Home page main section populated with placeholder text

--- chef-site/src/components/homeSection.js
+++ chef-site/src/components/homeSection.js
@@ -7,6 +7,21 @@ class HomeSec extends Component {
 			<img src="/Pictures/mediterranean-cuisine-2378758_1920.jpg" ></img>
       <h1>iCook4U Restaurant</h1>
 	  <h2>Some slogan here</h2>
+	  <div className="main-home-sec">
+		  <p>Lorem ipsum dolor sit amet, 
+			consectetur adipiscing elit, 
+			sed do eiusmod tempor 
+			incididunt ut labore et dolore 
+			magna aliqua. Porta nibh venenatis cras sed felis. Arcu dictum varius duis at. Ut ornare lectus sit amet est placerat. Semper viverra nam libero justo laoreet. Euismod quis viverra nibh cras. Leo in vitae turpis massa sed elementum tempus egestas sed. Laoreet id donec ultrices tincidunt arcu non. Maecenas volutpat blandit aliquam etiam erat velit. Aliquam nulla facilisi cras fermentum odio eu. Nam at lectus urna duis convallis. Rhoncus aenean vel elit scelerisque mauris pellentesque. Congue quisque egestas diam in arcu cursus. Porttitor eget dolor morbi non. Tempor id eu nisl nunc mi. Amet commodo nulla facilisi nullam.
+
+Leo a diam sollicitudin tempor id eu. Tempor orci dapibus ultrices in iaculis nunc sed augue. Feugiat pretium nibh ipsum consequat. Pretium fusce id velit ut tortor pretium viverra. Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique. Ullamcorper dignissim cras tincidunt lobortis. Ipsum dolor sit amet consectetur adipiscing elit. Nec tincidunt praesent semper feugiat nibh. Varius sit amet mattis vulputate enim nulla aliquet porttitor. Quis hendrerit dolor magna eget. Mattis ullamcorper velit sed ullamcorper morbi.
+
+Mi proin sed libero enim. Quam quisque id diam vel quam. Rhoncus dolor purus non enim praesent elementum facilisis leo vel. Faucibus et molestie ac feugiat sed lectus. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Aliquet lectus proin nibh nisl condimentum id. Cum sociis natoque penatibus et magnis dis parturient montes nascetur. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Porta non pulvinar neque laoreet suspendisse. Eget nunc lobortis mattis aliquam faucibus purus. Eget egestas purus viverra accumsan. Etiam tempor orci eu lobortis elementum. Amet est placerat in egestas erat imperdiet sed. Hendrerit dolor magna eget est lorem ipsum dolor sit amet. A lacus vestibulum sed arcu non. At imperdiet dui accumsan sit amet. Ut sem nulla pharetra diam sit.
+
+Integer enim neque volutpat ac tincidunt. Ultricies mi quis hendrerit dolor magna. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Lorem ipsum dolor sit amet consectetur adipiscing elit. Aliquam malesuada bibendum arcu vitae elementum curabitur. Sed turpis tincidunt id aliquet risus feugiat in. Quis commodo odio aenean sed adipiscing diam donec adipiscing tristique. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Leo integer malesuada nunc vel risus commodo. Posuere sollicitudin aliquam ultrices sagittis. Consectetur libero id faucibus nisl tincidunt eget nullam. Odio euismod lacinia at quis risus sed vulputate. Diam in arcu cursus euismod quis viverra nibh cras. Placerat duis ultricies lacus sed turpis. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Turpis massa sed elementum tempus egestas.
+
+Aliquet nec ullamcorper sit amet risus nullam eget felis. Sem viverra aliquet eget sit. Sed risus ultricies tristique nulla aliquet enim tortor at auctor. Lacus viverra vitae congue eu consequat. Sed euismod nisi porta lorem mollis aliquam ut porttitor. Gravida rutrum quisque non tellus orci ac. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis. Nunc sed velit dignissim sodales ut eu. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Odio aenean sed adipiscing diam donec adipiscing tristique. Ipsum nunc aliquet bibendum enim. Pulvinar etiam non quam lacus suspendisse. Diam quis enim lobortis scelerisque fermentum dui. Pellentesque habitant morbi tristique senectus. Proin nibh nisl condimentum id.</p>
+	  </div>
         </>
 		)
 	}
